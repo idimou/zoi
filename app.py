4ce17d497ca0be727f21f004c5a94e5644a1087b
idimou <jdimou@gmail.com>
2021-01-13 02:03:23 +0100

    pointed app.py to index.html

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     app.route('/')
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/about/')
 def about():
